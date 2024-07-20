be657950722e93e6851ffcf3355f55e30f3eea43
mrhacker077 <manjunathm7091@gamil.com>
2024-07-20 21:31:58 +0530

    first commit

---
+++ typescript/1.js
@@ -0,0 +1,5 @@
+var a = 10;
+var b = 30;
+var res;
+res = a + b;
+console.log("result of numbers" + res);
---
+++ typescript/1.ts
@@ -0,0 +1,5 @@
+let a:number=10;
+let b:number=30;
+let res:number;
+res=a+b;
+console.log("result of numbers"+res);
